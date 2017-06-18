21ab69aa006e722c37375d60f9677753a33ce403
Jonathan Lowell <elderlowell@gmail.com>
2017-06-18 11:11:07 -0600

    Completed lesson hider project.

--- js/lessonCtrl.js
+++ js/lessonCtrl.js
@@ -1,4 +1,13 @@
 angular.module('directivePractice').controller('lessonCtrl', function($scope) {
   $scope.lessons = ['Services','Routing','Directives','Review','Firebase','No server project','Node','Express','Mongo'];
   $scope.test = "Two-way data binding!";
+  $scope.announceDay = function(lesson, day) {
+    if (!day) {
+      alert(lesson + ' is not currently scheduled on any day.');
+    }
+    else {
+      alert(lesson + ' is active on ' + day + '.');
+    }
+  };
+  $scope.checked = false;
 });
--- js/lessonHider.js
+++ js/lessonHider.js
@@ -3,20 +3,37 @@ angular.module('directivePractice').directive('lessonHider', function() {
     templateUrl: './views/lessonHider.html',
     restrict: 'E',
     scope: {
-      lesson: '='
+      lesson: '=',
+      dayAlert: '&',
+      crossOut: '='
     },
     controller: function($scope, lessonService) {
       $scope.getSchedule = lessonService.getSchedule();
+      console.log(111111111, $scope.crossOut)
     },
     link: function (scope, element, attributes) {
+      console.log(222222222, scope.crossOut)
+
       scope.getSchedule.then(function(response) {
         scope.schedule = response.data;
-        scope.schedule.forEach(function(scheduleDay) {
-          if (scheduleDay.lesson === scope.lesson) {
-            element.css('text-decoration', 'line-through');
-            return;
-          }
-        });
+        crossOut();
+
+        scope.$watch('crossOut', crossOut);
+
+        function crossOut() {
+          scope.schedule.forEach(function(scheduleDay) {
+            if (scheduleDay.lesson === scope.lesson && scope.crossOut) {
+              element.css('text-decoration', 'line-through');
+              scope.lessonDay = scheduleDay.weekday;
+              return;
+            }
+            else if (scheduleDay.lesson === scope.lesson && !scope.crossOut)  {
+              element.css('text-decoration', 'none');
+              scope.lessonDay = scheduleDay.weekday;
+              return;
+            }
+          });
+        }
       });
     }
   }
